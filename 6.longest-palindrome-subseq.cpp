b365e2a049b182f223d2c5f97499e58bca26567e
archit-tiwari-26 <architt2604@gmail.com>
2026-05-01 00:59:37 +0530

    more questions

---
+++ 6.longest-palindrome-subseq.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+/*Given a string s, find the longest palindromic subsequence's length in s.
+A subsequence is a sequence that can be derived from another sequence by 
+deleting some or no elements without changing the order of the remaining elements.
+*/
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j], dp [i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
---
+++ 7.min-deletions-to-palindrome.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+//Given a string s, write a program to delete the minimum number of characters from the string
+ //so that the resultant string is a palindrome,
+  //while maintaining the order of characters.
+  
+class Solution {
+  public:
+    int minDeletions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j], dp [i][j-1]);
+                }
+            }
+        }
+        return  n-dp[n][n];
+        
+    }
+};
